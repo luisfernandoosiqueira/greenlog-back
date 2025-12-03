2a77a68db32bab1b43be00b259b08b328977e3d0
Luis Fernando <luisfernandosiqueiraadv@gmail.com>
2025-12-03 14:03:21 -0300

    feat: implementação de alguns regex nas classes RequestDTO

--- src/main/java/app/dto/auth/LoginRequestDTO.java
+++ src/main/java/app/dto/auth/LoginRequestDTO.java
@@ -1,13 +1,16 @@
 package app.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequestDTO(
 
         @NotBlank(message = "O nome de usuário é obrigatório")
+        @Size(max = 60, message = "O nome de usuário deve ter no máximo 60 caracteres")
         String username,
 
         @NotBlank(message = "A senha é obrigatória")
+        @Size(min = 6, max = 120, message = "A senha deve ter entre 6 e 120 caracteres")
         String senha
 ) {
 }
